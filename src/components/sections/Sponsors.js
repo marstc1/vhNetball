e2d06bf515d0245c07f1066beef142b3c70fe6c9
Chris Marston <christopher.marston@hotmail.co.uk>
2023-04-11 11:55:35 +0100

    Added SJP sponsor

--- src/components/sections/Sponsors.js
+++ src/components/sections/Sponsors.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import sjpLogo from "../../images/SJP_Logo_RGB_NAVY.svg";
 
 export default class Sponsors extends Component {
   render() {
     return (
-      <div id='sectionSponsors' className='section'>
+      <div id='sectionSponsors' className='section sponsors'>
         <div className='container'>
           <h2>Sponsors</h2>
-          <p>
-            Logo's to follow ...
-          </p>
+          <div style={{width: "100%", textAlign: "center"}}>
+            <a
+              href='https://www.sjp.co.uk/'
+              target='_blank'
+              rel='noopener noreferrer'>
+              <img src={sjpLogo} alt="St. James's Place" height='150px' />
+            </a>
+          </div>
         </div>
       </div>
     );
